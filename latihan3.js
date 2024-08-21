4401b18b754c12805c2659e0dbebd01b562c5b01
Ahmad Surya Alam Syah <alamsyah200527@gmail.com>
2024-08-21 08:39:59 +0700

    Final Intro

---
+++ latihan3.js
@@ -0,0 +1,4 @@
+const tambah = require('./math');
+
+const result = tambah(2, 3);
+console.log(`2 + 3 = ${result}`);
---
+++ math.js
@@ -0,0 +1,5 @@
+const tambah = (a, b) => {
+  return a + b;
+};
+
+module.exports = tambah;
