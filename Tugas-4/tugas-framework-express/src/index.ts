27fb63f16b0c9aa3f31650bab967f8a0f9ad6a28
Ahmad Surya Alam Syah <alamsyah200527@gmail.com>
2024-08-26 21:44:34 +0700

    update tugas-4

---
+++ Tugas-4/tugas-framework-express/src/index.ts
@@ -0,0 +1,46 @@
+import express, { Request, Response } from 'express';
+import path from 'path';
+const PORT = 3000;
+
+function init() {
+  const app = express();
+
+  app.use(express.static(path.join(__dirname, '..', 'public')));
+
+  app.use((req, res, next) => {
+    console.log(`${req.method} request to '${req.url}'`);
+    next();
+  });
+
+  app.get('/', (req: Request, res: Response) => {
+    res.status(200).json({
+      message: 'OK',
+      data: null,
+    });
+  });
+
+  app.get('/hello', (req: Request, res: Response) => {
+    res.status(200).json({
+      message: 'Success fetch message',
+      data: 'Hello World',
+    });
+  });
+
+  app.get('/user', (req: Request, res: Response) => {
+    res.status(200).json({
+      message: 'Success fetch user',
+      data: {
+        id: 1,
+        name: 'Budi',
+        username: 'budidu',
+        email: 'budidu@gmail.com',
+      },
+    });
+  });
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+  });
+}
+
+init();
